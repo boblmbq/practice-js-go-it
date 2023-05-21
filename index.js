0f91c4df4ccc1a1afbfa13c6aef8a2d3dd1ad311
boblmbq <sichkardanylo@gmail.com>
2023-05-21 10:35:18 +0200

    first code

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ *? Напишіть цикл, який виводить у консоль
+ *? числа  від max до min  за спаданням
+ *? Виведіть у консоль усі парних чисел від min до max
+ */
+
+const max = 50;
+const min = 23;
+
+for (let i = max; i >= min; i -= 1) {
+  console.log(i);
+}
+
+for (let i = min; i <= max; i += 1) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
