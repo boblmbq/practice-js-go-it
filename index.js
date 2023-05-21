eb93a502c8a7e9ec32bae5be36b86d206b7e3580
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-05-21 12:50:01 +0300

    Task4

--- index.js
+++ index.js
@@ -17,7 +17,6 @@
 //   }
 // }
 
-
 /**
  *? Використовуючи функцію if...else,
  *? напишіть код, який запитуватиме:
@@ -26,7 +25,6 @@
  *? то показати через alert: "Вірно!"
  *? інакше відобразити:"Не знаєте? ECMAScript!"
  */
-
 
 // const message = prompt("Яка офіційна назва JavaScript?").trim();
 
@@ -44,16 +42,40 @@
  70 === 01:10
  */
 
-const number = prompt("ведіть число");
-const hours = String(Math.floor(number/60)).padStart(2, "0");
-const minutes = String(number%60).padStart(2, "0");
-console.log(`${hours}:${minutes}`);
+// const number = prompt("ведіть число");
+// const hours = String(Math.floor(number / 60)).padStart(2, "0");
+// const minutes = String(number % 60).padStart(2, "0");
+// console.log(`${hours}:${minutes}`);
 
+/**
+ *? При завантаженні сторінки користувачеві пропонується
+ *? в prompt ввести число. Введення додається до значення
+ *? змінної total.
+ *? Операція введення числа триває до того часу,
+ *? поки користувач не натисне кнопку Cancel у prompt.
+ *? Після того як користувач припинив введення, натиснувши на
+ *? кнопку Cancel, показати alert з рядком "Загальна сума введених чисел дорівнює [число]."
+ 
+ *! Робити перевірку, що користувач ввів саме число,
+ *! а не довільний набір символів не потрібно.
+ */
 
+let total = 0;
+let number = prompt("Введить число?");
+// do {
+//   if (number) {
+//     total += Number(number);
 
+//     number = prompt("Введить число?");
+//   }
+// } while (number !== null);
 
+// console.log(total);
+// інший варіант
+while (number !== null);
+{
+  total += Number(number);
 
-
-
-
-
+  number = prompt("Введить число?");
+}
+console.log(total);
