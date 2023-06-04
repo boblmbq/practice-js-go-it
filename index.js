f3b5d8a83dfdf2dfbd0905ac6d052e4133b00ca4
boblmbq <sichkardanylo@gmail.com>
2023-06-04 10:10:06 +0200

    my comit

--- index.js
+++ index.js
@@ -4,15 +4,17 @@
  *? Виведіть у консоль усі парних чисел від min до max
  */
 
-const max = 50;
-const min = 23;
+// const max = 50;
+// const min = 23;
 
-for (let i = max; i >= min; i -= 1) {
-  console.log(i);
-}
+// for (let i = max; i >= min; i -= 1) {
+//   console.log(i);
+// }
 
-for (let i = min; i <= max; i += 1) {
-  if (i % 2 === 0) {
-    console.log(i);
-  }
-}
+// for (let i = min; i <= max; i += 1) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+
